834a4ed5932ba4313984871d031a35ad41b986a5
Mike Parish <michaeljparish@gmail.com>
2020-03-03 09:23:24 -0500

    Events tutorial

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
         name: 'Mike',
         job: 'Ninja',
         website: 'https://google.com',
-        websiteTag: '<a href="https://google.com">The Net Ninja Website</a>' 
+        websiteTag: '<a href="https://google.com">The Net Ninja Website</a>',
+        age: 25,
+        x: 0,
+        y: 0,
     },
     methods: {
         greet: function(time){
             return 'Good ' + time + ' ' + this.name;
+        },
+        add: function(inc) {
+            this.age += inc;
+        },
+        sub: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 })
