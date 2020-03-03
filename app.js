e0bb5235d13b3396e8e19fa2c262975a6e4982d3
Mike Parish <michaeljparish@gmail.com>
2020-03-03 11:19:48 -0500

    Computer properties

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Mike',
+        name2: '',
         job: 'Ninja',
         website: 'https://google.com',
         websiteTag: '<a href="https://google.com">The Net Ninja Website</a>',
         age: 25,
+        age2: '',
         x: 0,
         y: 0,
+        a: 0,
+        b: 0,
+        age3: 20
     },
     methods: {
         greet: function(time){
@@ -26,5 +31,30 @@ new Vue({
         click: function() {
             alert('You clicked me');
         },
-    }
+        logName: function() {
+            console.log('You entered your name');
+        },
+        logAge: function() {
+            console.log('You entered your age');
+        },
+        /*
+        addToA: function() {
+            console.log('addToA');
+            return this.a + this.age3;
+        },
+        addToB: function() {
+            console.log('addToB');
+            return this.b + this.age3;
+        },*/
+    },
+    computed: {
+        addToA: function() {
+            console.log('addToA');
+            return this.a + this.age3;
+        },
+        addToB: function() {
+            console.log('addToB');
+            return this.b + this.age3;
+        },
+    }  
 })
